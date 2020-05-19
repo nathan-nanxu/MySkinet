3495d3eeda2985903d3190103e14f5a0aa71de7e
Nathan Xu <nathan.xu@safecode.com.au>
2020-05-19 14:54:36 +1000

    End of section 5 API Error Handling

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -8,32 +8,42 @@ using Microsoft.Extensions.Logging;
 namespace API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route ("[controller]")]
+    [ApiExplorerSettings (IgnoreApi = true)]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static readonly string[] Summaries = new []
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing",
+            "Bracing",
+            "Chilly",
+            "Cool",
+            "Mild",
+            "Warm",
+            "Balmy",
+            "Hot",
+            "Sweltering",
+            "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController (ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<WeatherForecast> Get ()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var rng = new Random ();
+            return Enumerable.Range (1, 5).Select (index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays (index),
+                        TemperatureC = rng.Next (-20, 55),
+                        Summary = Summaries[rng.Next (Summaries.Length)]
+                })
+                .ToArray ();
         }
     }
 }
